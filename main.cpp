e8d12e5d9ebe1264666f71895bee8addb8458a24
UT0911 <giantprogrampanda33@gmail.com>
2026-04-19 16:14:29 +0900

    commit test

--- main.cpp
+++ main.cpp
@@ -1,6 +1,5 @@
 #include "DxLib.h"
 
-// プログラムは WinMain から始まります
 int WINAPI WinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPSTR lpCmdLine, int nCmdShow)
 {
 	if (DxLib_Init() == -1) //DXライブラリ初期化処理
